a08d1c7892d77cd63fe911406aef5f71bad6f962
Antonio Iorio <antonioiorio94@gmail.com>
2022-03-25 14:37:15 +0100

    avvio di Vue e assegnazione proprietà all'indice

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 console.log("JS OK");
+
+//avvio di Vue sull'id app
+
+const app = new Vue({
+  el: "#app",
+  data: {
+    slides,
+    //assegno manualmente una proprietà all'indice dell'array
+    activeSlideIndex: 2,
+  },
+});
